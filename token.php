2c8ea9bf298ef58c3a25f7296de218223ceb2f06
manumagique <36519659+manumagique@users.noreply.github.com>
2019-04-03 10:20:13 -0400

    integration de oAuth et des controller requis

---
+++ token.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emmanuelboyer
+ * Date: 2019-04-01
+ * Time: 11:54
+ */
+
+// include our OAuth2 Server object
+require_once __DIR__.'/server.php';
+
+// Handle a request for an OAuth2.0 Access Token and send the response to the client
+$server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
