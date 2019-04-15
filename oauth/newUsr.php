48cab52331423e446ffe1afe359f7f2fbe05708e
manumagique <36519659+manumagique@users.noreply.github.com>
2019-04-15 11:15:42 -0400

    UserName & Password oAuth 2 Login

---
+++ oauth/newUsr.php
@@ -0,0 +1,5 @@
+<?php
+// include our OAuth2 Server object
+require_once __DIR__ . '/server.php';
+
+$storage->setUser("john","123456", "pierre", "jean");
---
+++ oauth/server.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emmanuelboyer
+ * Date: 2019-04-01
+ * Time: 11:02
+ */
+
+use OAuth2\GrantType\UserCredentials;
+
+$dsn      = 'mysql:dbname=lango721_gestiondecommandes;host=69.28.199.20';
+$username = 'lango721_inm5001';
+$password = '?h17b+gplF;H';
+
+// error reporting (this is a demo, after all!)
+ini_set('display_errors',1);error_reporting(E_ALL);
+
+// Autoloading (composer is preferred, but for this example let's just do this)
+require_once('../oauth2-server-php/src/OAuth2/Autoloader.php');
+OAuth2\Autoloader::register();
+
+// $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
+$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+
+//$server = new OAuth2\Server($storage, array('allow_implicit' => true));
+// Pass a storage object or array of storage objects to the OAuth2 server class
+$server = new OAuth2\Server($storage);
+
+// Add the "Client Credentials" grant type (it is the simplest of the grant types)
+$server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+
+// Add the "Authorization Code" grant type (this is where the oauth magic happens)
+$server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+
+// create the grant type
+$server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
