4e91f7d465f1bcd83fd72e73114f4958e9c4c034
manumagique <36519659+manumagique@users.noreply.github.com>
2019-04-15 23:13:31 -0400

    some change to create user

--- oauth/newUsr.php
+++ oauth/newUsr.php
@@ -1,6 +1,8 @@
 <?php
 // include our OAuth2 Server object
 require_once __DIR__ . '/server.php';
-function newOauthUser(){
-    $storage->setUser("john","123456", "pierre", "jean");
-}
+$storage->setUser("emmanuel","123456", "jade", "Pomerleau Gauthier");
+
+//function newOauthUser(){
+//    $storage->setUser("jade","123456", "jade", "Pomerleau Gauthier");
+//}
