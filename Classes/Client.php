e93b8ee609f4ebc8ec16100edcf4b7580b412688
ValentinaLz <lzvalentina868@gmail.com>
2019-04-17 11:21:46 -0400

    Order retourne produits et quantite + retour pour post put delete

--- Classes/Client.php
+++ Classes/Client.php
@@ -17,6 +17,7 @@ class Client
 
     /* méthodes pour GET */
 
+    /* ------ GET details Client ------ */
     public function getClientDetails() {
 
         $db = Database::getInstance();
@@ -25,6 +26,7 @@ class Client
 
     }
 
+    /* ------ GET liste des produits du fournisseur rattache au Client ------ */
     public function getProductsList() {
 
         $db = Database::getInstance();
@@ -42,6 +44,7 @@ class Client
 
     }
 
+    /* ------ GET la fiche detaille d'un produit du fournisseur rattache au Client ------ */
     public function getProductDetails($idAbout) {
 
         $db = Database::getInstance();
@@ -58,6 +61,7 @@ class Client
 
     }
 
+    /* ------ GET liste des utilisateurs du Client ------ */
     public function getUsersList() {
 
         $db = Database::getInstance();
@@ -66,6 +70,7 @@ class Client
 
     }
 
+    /* ------ GET details d'un utilisateur du Client ------ */
     public function getUser($idAbout) {
 
         $db = Database::getInstance();
@@ -74,24 +79,49 @@ class Client
 
     }
 
+    /* ------ GET liste des commandes du Client ------ */
     public function getOrdersList() {
 
+        //$db = Database::getInstance();
+        //$db->query("SELECT * FROM ClientOrder WHERE fkidClient = ? ", array($this->_id));
+        //return $db->resultsToJson();
+
         $db = Database::getInstance();
-        $db->query("SELECT * FROM ClientOrder WHERE fkidClient = ? ", array($this->_id));
+        $db->query("SELECT ClientOrder.id, ClientOrder.date, ClientOrder.user, ClientOrder.commentaire, 
+       ClientOrder.status, ClientOrder.fkidClient, ClientOrder.fkidSupplier, clientOrderDetail.Qty, 
+       Product.idProduct, Product.nom, Product.prix, Product.description, Product.origine, Product.code, Product.format, Product.fkidSupplier
+                  FROM ClientOrder 
+                    INNER JOIN clientOrderDetail ON ClientOrder.id = clientOrderDetail.fkid_ClientOrder
+                      INNER JOIN Product ON Product.idProduct = clientOrderDetail.fkidProduct
+                        WHERE ClientOrder.fkidClient = ? ", array($this->_id));
+
         return $db->resultsToJson();
 
     }
 
+    /* ------ GET details d'une commande du Client ------ */
     public function getOrder($idAbout) {
 
+        //$db = Database::getInstance();
+        //$db->query("SELECT * FROM ClientOrder WHERE fkidClient = ? AND id = ?", array($this->_id, $idAbout));
+        //return $db->resultsToJson();
+
         $db = Database::getInstance();
-        $db->query("SELECT * FROM ClientOrder WHERE fkidClient = ? AND id = ?", array($this->_id, $idAbout));
+        $db->query("SELECT ClientOrder.id, ClientOrder.date, ClientOrder.user, ClientOrder.commentaire, 
+       ClientOrder.status, ClientOrder.fkidClient, ClientOrder.fkidSupplier, clientOrderDetail.Qty, 
+       Product.idProduct, Product.nom, Product.prix, Product.description, Product.origine, Product.code, Product.format, Product.fkidSupplier
+                  FROM ClientOrder 
+                    INNER JOIN clientOrderDetail ON ClientOrder.id = clientOrderDetail.fkid_ClientOrder
+                      INNER JOIN Product ON Product.idProduct = clientOrderDetail.fkidProduct
+                        WHERE ClientOrder.fkidClient = ? AND id = ?", array($this->_id, $idAbout));
+
         return $db->resultsToJson();
 
     }
 
     /* méthodes pour POST */
 
+    /* ------ POST ajout details d'un utilisateur du Client ------ */
     public function addUser($data) {
 
         $db = Database::getInstance();
@@ -111,41 +141,87 @@ class Client
 //            'scope' => $data->scope
         );
 
-        $db->insert('oauth_users', $fields);
+        $res = $db->insert('oauth_users', $fields);
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
     }
 
+    /* ------ POST ajout details d'une commande du Client ------ */
     public function addOrder($data) {
 
         $db = Database::getInstance();
 
+        $dateTime = date("Y-m-d");      //date("d/m/Y");
+
         $fields = array(
-            'date' => $data->date,
-            'user' => $data ->user,
-            'commentaire' => $data ->commentaire,
+            'date' => $dateTime,
+            'user' => $data->user,
+            'commentaire' => $data->commentaire,
             'status' => "0",
             'fkidClient' => $data->fkidClient,
             'fkidSupplier' => $data ->fkidSupplier,
         );
 
-        $db->insert('ClientOrder', $fields);
+        $res = $db->insert('ClientOrder', $fields);
 
-        $products = $data->produits;
+        if($res)
+        {
+            $db->query("SELECT id FROM ClientOrder WHERE date = ? AND user = ? 
+                             AND commentaire = ? AND status = ? AND fkidClient = ? 
+                             AND fkidSupplier = ?", array($dateTime, $data->user, $data->commentaire, "0",
+                $data->fkidClient, $data ->fkidSupplier));
 
-        foreach($products as $champ) {
-            $fields2 = array(
-                'fkidProduct' => $champ->fkidProduct,
-                'Qty' => $champ->Qty,
-                'fkid_ClientOrder' => $champ->fkid_ClientOrder
-            );
+            $_data = $db->results();
+            foreach ($_data as $row)
+            {
+                $idOrder = $row->id;
+            }
 
-            $db->insert('clientOrderDetail', $fields2);
+            $products = $data->produits;
+
+            foreach($products as $champ) {
+                $fields2 = array(
+                    'fkidProduct' => $champ->fkidProduct,
+                    'Qty' => $champ->Qty,
+                    'fkid_ClientOrder' => $idOrder
+                );
+
+                $res = $db->insert('clientOrderDetail', $fields2);
+
+            }
+
+            if($res)
+            {
+                return "Success";
+            }
+            else
+            {
+                return "Error";
+
+            }
 
         }
+        else
+        {
+            return "Error";
+
+        }
+
+
 
     }
 
     /* méthodes pour PUT */
 
+    /* ------ PUT mise à jour details d'un utilisateur du Client ------ */
     public function updateUser($data, $idAbout) {
 
         $db = Database::getInstance();
@@ -167,14 +243,25 @@ class Client
         //    'email' => $data->email
         //);
 
-        $db->update('oauth_users', $idAbout, $field);
+        $res = $db->update('oauth_users', $idAbout, $field);
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
     }
 
+    /* ------ PUT mise à jour details d'une commande du Client ------ */
     public function updateOrder($data, $idAbout) {
 
         $db = Database::getInstance();
 
-        $products = array();
+        //$products = array();
 
         $field = array();
         foreach ($data as $key => $value) {
@@ -185,7 +272,17 @@ class Client
             $field = $field + $fields;
         }
 
-        $db->update('ClientOrder', $idAbout, $field);
+        $res = $db->update('ClientOrder', $idAbout, $field);
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
 
 
         //$fields = array(
@@ -199,47 +296,74 @@ class Client
 
     /* méthodes pour DELETE */
 
-    public function deleteAllProducts() {
-
-        $db = Database::getInstance();
-
-        $clientSupplier = $db->query("SELECT fkidSupplier FROM Client WHERE idClient = ?", array($this->_id));
-        $db->query("DELETE FROM Product WHERE fkidSupplier = ? ", array($clientSupplier));
-
-    }
-
-    public function deleteProduct($idAbout) {
-
-        $db = Database::getInstance();
-        $clientSupplier = $db->query("SELECT fkidSupplier FROM Client WHERE idClient = ?", array($this->_id));
-        $db->query("DELETE FROM Product WHERE fkidSupplier = ? AND idProduct = ?", array($clientSupplier, $idAbout));
-
-    }
-
+    /* ------ DELETE les utilisateurs du Client ------ */
     public function deleteAllUsers() {
 
         $db = Database::getInstance();
-        $db->query("DELETE FROM oauth_users WHERE fkidClient = ? ", array($this->_id));
+        $res = $db->query("DELETE FROM oauth_users WHERE fkidClient = ? ", array($this->_id));
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
 
     }
 
+    /* ------ DELETE un utilisateur du Client ------ */
     public function deleteUser($idAbout) {
 
         $db = Database::getInstance();
-        $db->query("DELETE FROM oauth_users WHERE fkidClient = ? AND id = ? ", array($this->_id, $idAbout));
+        $res = $db->query("DELETE FROM oauth_users WHERE fkidClient = ? AND id = ? ", array($this->_id, $idAbout));
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
 
     }
 
+    /* ------ DELETE les commandes du Client ------ */
     public function deleteAllOrders() {
 
         $db = Database::getInstance();
-        $db->query("DELETE FROM ClientOrder WHERE fkidClient = ? ", array($this->_id));
+        $res = $db->query("DELETE FROM ClientOrder WHERE fkidClient = ? ", array($this->_id));
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
 
     }
 
+    /* ------ DELETE une commande du Client ------ */
     public function deleteOrder($idAbout) {
 
         $db = Database::getInstance();
-        $db->query("DELETE FROM ClientOrder WHERE fkidClient = ? AND id = ? ", array($this->_id, $idAbout));
+        $res = $db->query("DELETE FROM ClientOrder WHERE fkidClient = ? AND id = ? ", array($this->_id, $idAbout));
+
+        if($res)
+        {
+            return "Success";
+        }
+        else
+        {
+            return "Error";
+
+        }
     }
 }
